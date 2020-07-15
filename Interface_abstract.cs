668c159569d21ea8901b3c2e6e243cc69dc92034
Raj Sonam <sonamraja8946@gmail.com>
2020-07-15 11:15:30 +0530

    commit

---
+++ Interface_abstract.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Default
+{
+ public   class Stud
+    {
+        public void show()
+        {
+            Console.WriteLine("Hello!!!");
+        }
+    }
+    abstract class Stude : Stud
+    {
+        
+
+    }
+    public class Interface_abstract
+    {
+        
+    }
+}
