2845c979414c1aed6ebb3dad0e62a356e7a017fa
Raj Sonam <sonamraja8946@gmail.com>
2020-07-16 14:59:04 +0530

    commit

--- Hashtablee.cs
+++ Hashtablee.cs
@@ -13,7 +13,25 @@ namespace Default
             ht.Add("NAME","RAJ");
             ht.Add("SALARY",300000);
             ht.Add("DESIGNATION","MANAGER");
-            Console.WriteLine(ht["ID"]);
+         //   Console.WriteLine(ht["ID"]);
+
+            // foreach (object key in ht.Keys)
+            // {
+            //     Console.WriteLine(key+" :"+ ht[key]);
+            // }
+           // ht.Remove("SALARY");
+        //    ht.Clear();
+        //     Console.WriteLine("--------------");
+        //      foreach (object key in ht.Keys)
+        //     {
+        //         Console.WriteLine(key+" :"+ ht[key]);
+        //     }
+
+        Console.WriteLine(ht.Contains("ID"));
+        Console.WriteLine(ht.ContainsKey("ID"));
+        Console.WriteLine(ht.ContainsValue("KITE"));
+        Console.WriteLine(ht.Count);
+        Console.WriteLine("NAMe".GetHashCode());
         }
     }
 }
