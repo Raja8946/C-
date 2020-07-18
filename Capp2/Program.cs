dc81acfb6ea55e6fd3b130cb6a48c0e49bab2b39
Raj Sonam <sonamraja8946@gmail.com>
2020-07-18 20:32:20 +0530

    commit

---
+++ Capp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using cl2;
+using System.Collections.Generic;
+
+namespace Capp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+           Class1 employee1=new Class1()
+           {
+               Name="Raj",
+               age=20,
+           };
+
+           Class1 employee2=new Class1()
+           {
+               Name="Sonam",
+               age=21,
+           };
+           List<Class1> Mylist=new List<Class1>();
+           Mylist.Add(employee1);
+           Mylist.Add(employee2);
+           foreach(Class1 emp in Mylist)
+           {
+               Console.WriteLine("Employee name is:{0} and Employee age is :{1}",emp.Name,emp.age);
+           }
+
+           
+          // Console.WriteLine(employee.age);
+           
+        }
+    }
+}
---
+++ cl2/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace cl2
+{
+    public class Class1
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
