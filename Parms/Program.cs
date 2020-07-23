acb242e620025cdb5967ebf0545331f9b992f6f5
Raj Sonam <sonamraja8946@gmail.com>
2020-07-23 07:41:56 +0530

    commit

---
+++ Parms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Parms
+{
+    class Program
+    {
+        public static int Add(params int[] num)
+        {
+            int sum=0;
+            foreach (int i in num)
+            {
+                sum=sum+i;
+
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Add(10,20,30,40,50));
+        }
+    }
+}
