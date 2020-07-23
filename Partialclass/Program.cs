892353d6d6659e30687ad1cb79385ce3d5ced012
Raj Sonam <sonamraja8946@gmail.com>
2020-07-23 07:29:36 +0530

    commit

---
+++ Partialclass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+
+namespace Partialclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Class1 obj = new Class1();
+            // obj.Fname="Raj";
+            // obj.Lname="Sonam";
+            // obj.Getname();
+            partial obj = new partial();
+        }
+    }
+}
